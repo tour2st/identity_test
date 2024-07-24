d3c2f8fc37bf5f7b867880b7d8dba9011bf046bf
dyoshioka-555 <yoshioka.daiki@g.sp.m.is.nagoya-u.ac.jp>
2024-07-24 22:02:54 +0900

    add files

--- make_set_multi.py
+++ make_set_multi.py
@@ -3,13 +3,15 @@ from random import shuffle
 
 # 文字列形式でテキストと方法の組み合わせを生成
 texts = [
-    "A01M0906_0094625_0098470",
-    "A05F0756_0200498_0210897",
-    "A05M0058_1965542_1972876",
-    "S00F0063_0239050_0247330",
-    "S08M0639_0440306_0446946",
-]  # 5種類のテキスト
-methods = ["Plain", "Auto", "Gen", "Ctrl"]  # 4種類の方法
+    "A01F0001",
+    "A01F0019",
+    "A01F0034",
+    "A01F0055",
+    "A01F0067",
+    "A01M0007",
+    "A01M0008",
+]  # 7種類のテキスト
+methods = ["Ori", "Tran"]  # 2種類の方法
 combinations = list(itertools.permutations(methods, 2))  # 順序を考慮した組み合わせ
 
 set = [[], []]
@@ -19,12 +21,7 @@ set[1] = {"a": [], "b": [], "c": []}
 # 各テキストに対して異なる順序でAB、AC、BCのペアを作成
 for text, i in zip(texts, range(len(texts))):
     pairs = [
-        ["Plain", "Auto"],
-        ["Plain", "Gen"],
-        ["Plain", "Ctrl"],
-        ["Auto", "Gen"],
-        ["Auto", "Ctrl"],
-        ["Gen", "Ctrl"],
+        ["Ori", "Tran"],
     ]
     shuffle(pairs)
 
@@ -32,21 +29,25 @@ for text, i in zip(texts, range(len(texts))):
     for pair in pairs:
         shuffle(pair)
         if i % 2:
-            set[0]["a"].append(f"wav/set1/{pair[0]}/{text}.wav")
-            set[0]["b"].append(f"wav/set1/{pair[1]}/{text}.wav")
-            set[0]["c"].append(f"wav/set1/Ref/{text.split('_')[0]}.wav")
+            set[0]["a"].append(f"wav/set1/{pair[0]}/csj_{text}.wav")
+            set[0]["b"].append(f"wav/set1/{pair[1]}/csj_{text}.wav")
+            set[0]["c"].append(f"wav/set1/Ref/ref-{text}.wav")
 
-            set[1]["a"].append(f"wav/set2/{pair[1]}/{text}.wav")  # 順番を逆にして追加
-            set[1]["b"].append(f"wav/set2/{pair[0]}/{text}.wav")
-            set[1]["c"].append(f"wav/set2/Ref/{text.split('_')[0]}.wav")
+            set[1]["a"].append(
+                f"wav/set2/{pair[1]}/csj_{text}.wav"
+            )  # 順番を逆にして追加
+            set[1]["b"].append(f"wav/set2/{pair[0]}/csj_{text}.wav")
+            set[1]["c"].append(f"wav/set2/Ref/ref-{text}.wav")
         else:
-            set[0]["a"].append(f"wav/set1/{pair[1]}/{text}.wav")
-            set[0]["b"].append(f"wav/set1/{pair[0]}/{text}.wav")
-            set[0]["c"].append(f"wav/set1/Ref/{text.split('_')[0]}.wav")
+            set[0]["a"].append(f"wav/set1/{pair[1]}/csj_{text}.wav")
+            set[0]["b"].append(f"wav/set1/{pair[0]}/csj_{text}.wav")
+            set[0]["c"].append(f"wav/set1/Ref/ref-{text}.wav")
 
-            set[1]["a"].append(f"wav/set2/{pair[0]}/{text}.wav")  # 順番を逆にして追加
-            set[1]["b"].append(f"wav/set2/{pair[1]}/{text}.wav")
-            set[1]["c"].append(f"wav/set2/Ref/{text.split('_')[0]}.wav")
+            set[1]["a"].append(
+                f"wav/set2/{pair[0]}/csj_{text}.wav"
+            )  # 順番を逆にして追加
+            set[1]["b"].append(f"wav/set2/{pair[1]}/csj_{text}.wav")
+            set[1]["c"].append(f"wav/set2/Ref/ref-{text}.wav")
 
 
 for i in [1, 2]:
